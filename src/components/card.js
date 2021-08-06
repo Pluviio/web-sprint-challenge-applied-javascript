0a27ab194fdcb7f4a14be79d7379119bdf3cd0d8
courtneyharris <harriscourtney98@gmail.com>
2021-08-06 19:40:35 -0400

    Chocolate comes from the cacao plant

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,94 @@
+import axios from 'axios';
+// const Card = (article) => {
+//   // TASK 5
+//   // ---------------------
+//   // Implement this function, which should return the markup you see below.
+//   // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+//   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+//   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//   // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+//   //
+//   // <div class="card">
+//   //   <div class="headline">{ headline }</div>
+//   //   <div class="author">
+//   //     <div class="img-container">
+//   //       <img src={ authorPhoto }>
+//   //     </div>
+//   //     <span>By { authorName }</span>
+//   //   </div>
+//   // </div>
+//   //
+// }
+
+// const cardAppender = (selector) => {
+//   // TASK 6
+//   // ---------------------
+//   // Implement this function that takes a css selector as its only argument.
+//   // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
+//   // However, the articles do not come organized in a single, neat array. Inspect the response closely!
+//   // Create a card from each and every article object in the response, using the Card component.
+//   // Append each card to the element in the DOM that matches the selector passed to the function.
+//   //
+// }
+
+// export { Card, cardAppender }
+
 const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
+
+  //Card
+  const card = document.createElement('div')
+  card.addEventListener('click', () => {
+    card.classList.toggle('selected')
+
+    console.log('.headline');
+  })
+  card.classList.add('card')
+
+  //Headline
+  const articleHeadline = document.createElement('div')
+  articleHeadline.textContent = article.headline;
+  articleHeadline.classList.add('headline');
+  card.appendChild(articleHeadline);
+
+  //Author
+  const articleAuthor = document.createElement('div')
+  articleAuthor.classList.add('author');
+  card.appendChild(articleAuthor);
+
+  //Photo
+  const articlePhoto = document.createElement('div')
+
+  articlePhoto.classList.add('img-container');
+  articleAuthor.appendChild(articlePhoto);
+  const photoElement = document.createElement('img')
+  photoElement.src = article['authorPhoto'];
+  articlePhoto.appendChild(photoElement)
+
+  //Author Name
+  const articleName = document.createElement('span')
+  articleName.textContent = `By: ${article['authorName']}`
+  // cardAuthName.classList.add(cardAuthName);
+  articleAuthor.appendChild(articleName);
+
+
+  return card
+
+
 }
 
+
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
+axios.get(`http://localhost:5000/api/articles`)
+ .then(res => {
+   Object.keys(res.data.articles).forEach(section => {
+     console.log(section);
+     res.data.articles[section].forEach(article => {
+       console.log(article);
+       document.querySelector(selector).appendChild(Card(article))
+     })
+   })
+ })
+
 }
 
 export { Card, cardAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,65 @@
+import axios from 'axios';
+
+// const Tabs = (topics) => {
+//   // TASK 3
+//   // ---------------------
+//   // Implement this function which takes an array of strings ("topics") as its only argument.
+//   // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
+//   // then the function returns the markup below.
+//   // The tags used, the hierarchy of elements and their attributes must match the provided markup!
+//   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//   //
+//   // <div class="topics">
+//   //   <div class="tab">javascript</div>
+//   //   <div class="tab">bootstrap</div>
+//   //   <div class="tab">technology</div>
+//   // </div>
+//   //
+// }
+
+// const tabsAppender = (selector) => {
+//   // TASK 4
+//   // ---------------------
+//   // Implement this function which takes a css selector as its only argument.
+//   // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
+//   // Find the array of topics inside the response, and create the tabs using the Tabs component.
+//   // Append the tabs to the element in the DOM that matches the selector passed to the function.
+//   //
+// }
+
+// export { Tabs, tabsAppender }
+
+
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+
+  let codeTopics = document.createElement('div')
+
+  codeTopics.classList.add('topics')
+
+  topics.forEach(item => {
+
+    const tab = document.createElement('div');
+
+    tab.classList.add('tab')
+
+    tab.textContent = item;
+
+    codeTopics.appendChild(tab);
+
+  });
+
+  return codeTopics;
 }
 
+
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+  let container = document.querySelector(selector);
+
+  axios.get(`http://localhost:5000/api/topics`)
+  .then(res => {
+    container.appendChild(Tabs(res.data.topics))
+  })
 }
 
 export { Tabs, tabsAppender }
